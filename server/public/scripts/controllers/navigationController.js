e504f5ce9a75074857b398bf7d5715de784c7550
emilykeenan <emilyjanekeenan@gmail.com>
2016-12-09 09:37:55 -0600

    navigation controller running

---
+++ server/public/scripts/controllers/navigationController.js
@@ -0,0 +1,10 @@
+app.controller('NavigationController', ['$firebaseAuth', 'DataFactory', function($firebaseAuth, DataFactory) {
+
+  console.log('nav controller is running');
+
+  var self = this;
+
+  self.logOut = DataFactory.logOut;
+  self.currentUser = DataFactory.currentUser();
+
+}]);
