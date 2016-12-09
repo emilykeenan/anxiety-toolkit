4ea3f8b958257a4c6ef30e4cac115d9cbae7f91b
emilykeenan <emilyjanekeenan@gmail.com>
2016-12-09 17:06:27 -0600

    fixed drag and drop

--- server/public/scripts/controllers/dragDotController.js
+++ server/public/scripts/controllers/dragDotController.js
@@ -19,6 +19,66 @@ app.controller('DragDotController', ['$firebaseAuth', 'DataFactory', function($f
       id: "id-002",
       title: "item 002"
     },
+    {
+      id: "id-002",
+      title: "item 003"
+    },
+    {
+      id: "id-002",
+      title: "item 004"
+    },
+    {
+      id: "id-002",
+      title: "item 005"
+    },
+    {
+      id: "id-002",
+      title: "item 006"
+    },
+    {
+      id: "id-002",
+      title: "item 007"
+    },
+    {
+      id: "id-002",
+      title: "item 008"
+    },
+    {
+      id: "id-002",
+      title: "item 008"
+    },
+    {
+      id: "id-002",
+      title: "item 009"
+    },
+    {
+      id: "id-002",
+      title: "item 010"
+    },
+    {
+      id: "id-002",
+      title: "item 011"
+    },
+    {
+      id: "id-002",
+      title: "item 012"
+    },
+    {
+      id: "id-002",
+      title: "item 013"
+    },
+    {
+      id: "id-002",
+      title: "item 014"
+    },
+    {
+      id: "id-002",
+      title: "item 015"
+    },
+    {
+      id: "id-002",
+      title: "item 016"
+    }
   ];
 
 }]);
