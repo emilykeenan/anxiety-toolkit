4d50089c18f5dcd5c392921f2123cda18dbd596f
emilykeenan <emilyjanekeenan@gmail.com>
2016-12-16 13:33:24 -0600

    modified dot fidget

--- server/public/scripts/controllers/dragDotController.js
+++ server/public/scripts/controllers/dragDotController.js
@@ -5,9 +5,10 @@ app.controller('DragDotController', ['$firebaseAuth', 'DataFactory', function($f
   var self = this;
 
   self.currentUser = DataFactory.currentUser();
+  self.swappingDots = true;
+  self.fillingDots = false;
 
-  // list of colored dots
-  self.sortableList = [
+  self.rainbow = [
     {
       color: "hsla(170, 95%, 35%, 1)"
     },
@@ -172,6 +173,338 @@ app.controller('DragDotController', ['$firebaseAuth', 'DataFactory', function($f
     }
   ];
 
+  self.lightRainbow = [
+    {
+      color: "hsla(170, 95%, 35%, 0.5)"
+    },
+    {
+      color: "hsla(170, 95%, 45%, 0.5)"
+    },
+    {
+      color: "hsla(170, 95%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(170, 95%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(170, 95%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(170, 95%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(360, 95%, 35%, 0.5)"
+    },
+    {
+      color: "hsla(360, 95%, 45%, 0.5)"
+    },
+    {
+      color: "hsla(360, 95%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(360, 95%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(360, 95%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(360, 95%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(319, 95%, 35%, 0.5)"
+    },
+    {
+      color: "hsla(319, 95%, 45%, 0.5)"
+    },
+    {
+      color: "hsla(319, 95%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(319, 95%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(319, 95%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(319, 95%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(29, 95%, 45%, 0.5)"
+    },
+    {
+      color: "hsla(29, 95%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(29, 95%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(29, 95%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(29, 95%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(29, 95%, 95%, 0.5)"
+    },
+    {
+      color: "hsla(114, 45%, 45%, 0.5)"
+    },
+    {
+      color: "hsla(114, 45%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(114, 45%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(114, 45%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(114, 45%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(114, 45%, 95%, 0.5)"
+    },
+    {
+      color: "hsla(61, 95%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(61, 95%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(61, 95%, 70%, 0.5)"
+    },
+    {
+      color: "hsla(61, 95%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(61, 95%, 80%, 0.5)"
+    },
+    {
+      color: "hsla(61, 95%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(268, 85%, 45%, 0.5)"
+    },
+    {
+      color: "hsla(268, 85%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(268, 85%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(268, 85%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(268, 85%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(268, 85%, 95%, 0.5)"
+    },
+    {
+      color: "hsla(230, 45%, 45%, 0.5)"
+    },
+    {
+      color: "hsla(230, 45%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(230, 45%, 65%, 0.5)"
+    },
+    {
+      color: "hsla(230, 45%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(230, 45%, 85%, 0.5)"
+    },
+    {
+      color: "hsla(230, 45%, 95%, 0.5)"
+    },
+    {
+      color: "hsla(231, 5%, 15%, 0.5)"
+    },
+    {
+      color: "hsla(231, 5%, 35%, 0.5)"
+    },
+    {
+      color: "hsla(231, 5%, 55%, 0.5)"
+    },
+    {
+      color: "hsla(231, 5%, 75%, 0.5)"
+    },
+    {
+      color: "hsla(231, 5%, 85%, 0.5))"
+    },
+    {
+      color: "hsla(231, 5%, 95%, 0.5)"
+    }
+  ];
+
+  self.grayscale = [
+    {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 15%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 35%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 55%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 75%, 0.5)"
+  },
+  {
+    color: "hsla(231, 5%, 85%, 0.5))"
+  },
+  {
+    color: "hsla(231, 5%, 95%, 0.5)"
+  }
+];
+
+  self.sortableList = self.rainbow;
+
   var tempForSwap = null;
   var tempIndexForSwap = null;
 
@@ -189,6 +522,20 @@ app.controller('DragDotController', ['$firebaseAuth', 'DataFactory', function($f
     }
   }
 
+  self.fillDot = function(item, i) {
+    if(tempForSwap === null) {
+      tempForSwap = item;
+      tempIndexForSwap = i;
+    } else {
+      self.sortableList[i] = tempForSwap;
+    }
+  }
+
+  self.clearFillData = function() {
+    tempForSwap = null;
+    tempIndexForSwap = null;
+  }
+
   // Fisher-Yates Shuffle added to randomize order of colored dots
   function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -203,7 +550,34 @@ app.controller('DragDotController', ['$firebaseAuth', 'DataFactory', function($f
   return array;
 }
 
-self.sortableList = shuffle(self.sortableList);
-console.log(self.sortableList);
+self.shuffleDots = function() {
+  self.sortableList = shuffle(self.sortableList);
+}
+
+self.switchToLightRainbowTheme = function() {
+  self.sortableList = self.lightRainbow;
+}
+
+self.switchToRainbowTheme = function() {
+  self.sortableList = self.rainbow;
+}
+
+self.switchToGrayscaleTheme = function() {
+  self.sortableList = self.grayscale;
+}
+
+self.switchToFill = function() {
+  self.swappingDots = false;
+  self.fillingDots = true;
+  tempForSwap = null;
+  tempIndexForSwap = null;
+}
+
+self.switchToSort = function() {
+  self.fillingDots = false;
+  self.swappingDots = true;
+  tempForSwap = null;
+  tempIndexForSwap = null;
+}
 
 }]);
